f2f9d6310375dcc07e843bc015e5940c7e42bce8
leonardo.roldan1139 <leonardo.roldan1139@gmail.com>
2022-05-12 23:23:53 -0400

    frontend, conection backend

--- src/main/resources/static/js/serviceOfferDetail.js
+++ src/main/resources/static/js/serviceOfferDetail.js
@@ -2,67 +2,80 @@ console.log("Service");
 
 var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
-var anuncioParam = urlParams.get('id');
-console.log(anuncioParam);
-let property;
+var id = urlParams.get('id');
+console.log(id);
 
-
-async function getProperty() {
-    const request = await fetch('http://localhost:9091/properties/21',{
+var property = async function getProperty(id) {
+       const request = await fetch('http://localhost:9091/properties/' +id,{
         method: 'GET',
         headers: getHeaders()
     });
     const property = await request.json();   
     return property;
 }
+
 window.onload = async function () {
-    console.log(getProperty());
-    property = getProperty();
-    console.log(property);
+    property(id).then(property=>buildDetail(property))
 
 };
 
-//http://localhost:9091/properties/21
-//const OfferDetail = document.querySelector("#offerDetail");
+async function getFiles(propertyId) {
+    const requestFiles = await fetch('http://localhost:9091/properties/' + propertyId, {
+        method: 'GET',
+        headers: getHeaders()
+    });
+    const files = await requestFiles.json();
+    console.log(files.photos[0]);
+    
+    var srcImg = "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
+    //if (files.photos.length > 0) {
+     //   const fileObj = files.photographs[0];
+      //  console.log(fileObj);
+       // srcImg = "data:" + fileObj.mimeType + ";base64," + fileObj.value;
+   // }
+    return srcImg;
+}
 
-//let div = createDetail();
-//console.log(div);
-//let div2 = document.createElement('div');
-//document.querySelector("#offerDetail").appendChild(div2);
-//document.querySelector("#offerDetail").innerHTML = "<h1>No existen ofertas diponibles por el momento</h1>"
-//OfferDetail.innerHTML = div;
+async function buildDetail(props){ 
+    console.log(props);
+    const image =  getFiles(id);
+    const detail  = createDetail(props,image);
+    let elemento = document.getElementById("offerDetail");
+    elemento.innerHTML =  `${detail}`;
+}
 
-
-
-function createDetail(){
+function createDetail(prop,src){
+    //console.log(prop);
     let cardHtml = 
-        '<h3 class="text-center pt-5">Título inmueble</h3>' +
+        '<h3 class="text-center pt-5">' + prop.propertyDto.title+ '</h3>' +
         '<div class="container pt-5">'+
         '    <div class="row pt-5">'+
         '        <div class="col-6 text-center">'+
-        '            <img src="https://firebasestorage.googleapis.com/v0/b/alquiler-a732a.appspot.com/o/casa02.jpg?alt=media&token=a650e6d0-6e17-4509-a6d6-8ae2a1e9d7b3" width="420px">'+
-        '            <h5>Zona / Ciudad</h5>'+
+        '            <img src='+src+' width="420px">'+
+        '            <h5>'+ prop.propertyDto.zone+'/'+ prop.propertyDto.propertyDepartment+'</h5>'+
         '        </div>'+
         '            <div class="col-6 pt-2 ">'+
         '            <div class="row">'+
         '                <div class="col-4"><h5>Precio:</h5></div>'+
-        '                <div class="col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, rerum.</div>'+
+        '                <div class="col-8">'+prop.propertyDto.price +'</div>'+
         '            </div>'+
         '            <div class="row">'+
         '                <div class="col-4"><h5>Descripción:</h5></div>'+
-        '                <div class="col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate porro placeat beatae maxime atque quia facere unde quidem numquam id.</div>'+
+        '                <div class="col-8">'+ prop.propertyDto.description+'</div>'+
         '            </div>   '+
         '            <div class="row">'+
         '                <div class="col-4"><h5>Tipo de Imnueble:</h5></div>'+
-        '                <div class="col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>'+
+        '                <div class="col-8">' +prop.propertyDto.propertyType + '</div>'+
         '            </div>   '+
         '            <div class="row">'+
         '                <div class="col-4"><h5>Direccíon:</h5></div>'+
-        '                <div class="col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>'+
+        '                <div class="col-8">'+ prop.propertyDto.address +'</div>'+
         '            </div>'    +          
         '        </div>'+
         '    </div>'+
-        '</div>'
+        '</div>';
+
+
     return cardHtml;
 }
 
