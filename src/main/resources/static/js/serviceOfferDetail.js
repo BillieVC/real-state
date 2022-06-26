02881b9fab7c87bbd3e5ad543c7bcae9bc208759
leonardo.roldan1139 <leonardo.roldan1139@gmail.com>
2022-06-26 18:14:56 -0400

    pop up con formulario implementado

--- src/main/resources/static/js/serviceOfferDetail.js
+++ src/main/resources/static/js/serviceOfferDetail.js
@@ -170,3 +170,18 @@ function getHeaders() {
         'Content-Type': 'application/json'
     };
 }
+
+function fechaLimitada(){
+    var dtToday = new Date();
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate()+1;
+    var year = dtToday.getFullYear();
+    if(month < 10)
+    month = '0' + month.toString();
+    if(day < 10){
+        day = '0' + day.toString();
+    }
+    var maxDate = year + '-' + month + '-' + day;
+    const elemento = document.getElementById("txtDate").setAttribute("min", maxDate);
+}  
+fechaLimitada();
