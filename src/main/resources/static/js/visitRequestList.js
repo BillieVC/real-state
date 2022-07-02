1449799e34dcee1645c6ee6dcf34fde6ecc6a4d4
Leonardo1139 <leonardo.roldan1139@gmail.com>
2022-07-02 14:30:26 -0400

    visit list table implemented

---
+++ src/main/resources/static/js/visitRequestList.js
@@ -0,0 +1,47 @@
+const SEPARATOR = "T"; 
+queryString = window.location.search;
+urlParams = new URLSearchParams(queryString);
+propertyId = urlParams.get('id');
+
+let visitRequestList = async function getVisitRequestList(propertyId) {
+    let request = await  fetch('/appointments/' + propertyId, {
+        method: 'GET',
+        headers: getHeaders()
+    });
+    return await request.json();
+}
+
+
+async function getListFromDataBase() {
+    let visitListJsonFormat = await visitRequestList(propertyId);
+    createVisitRequestList(visitListJsonFormat.appointmentDtoList);
+};
+
+function getHeaders() {
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    };
+}
+
+getListFromDataBase();
+
+function createVisitRequestList(List){
+    for (let index = 0; index < List.length; index++) {
+          let element = List[index];  
+          let dateAndHour = element.date.split(SEPARATOR);
+          let date = dateAndHour[0];
+          let hour = dateAndHour[1];
+          let time = hour.split(".");
+          let hourWithoutSeconds = time[0]; 
+        document.getElementById("listRequestVisitTable").innerHTML += '<tr>'+
+                                                    '<th scope="row">'+element.id+'</th>'+
+                                                    '<td>'+element.userName+'</td>'+
+                                                    '<td>'+element.userEmail+'</td>'+
+                                                    '<td>'+element.userPhone+'</td>'+
+                                                    '<td>'+date+'</td>'+
+                                                    '<td>'+hourWithoutSeconds+'</td>'+
+                                                '</tr>'
+    }
+}
+
